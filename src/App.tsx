ddfd21b704530c9598abb090987be109b5823cb1
thisisabhinay <a8h1nay@gmail.com>
2022-07-28 01:28:26 +0530

    Setup vite and initialized react app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "@/App.sass"
+import Card from "@components/card/card"
+
+function App() {
+    return (
+        <div className="App">
+            New Content Here...
+            <Card />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/card/card.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import "@components/card/card.sass"
+
+function Card() {
+    return (
+        <div>Card</div>
+    )
+}
+
+export default Card
